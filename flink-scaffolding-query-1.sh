2492d96b1aa04fc1f8e4ff026b7a81eb2d3fa9e1
Giacomo Marciani <giacomo.marciani@gmail.com>
2017-06-19 19:19:07 +0200

    important fixes

--- flink-scaffolding-query-1.sh
+++ flink-scaffolding-query-1.sh
@@ -13,7 +13,7 @@ FLINK_RUN="${FLINK_HOME}/bin/flink run"
 ##
 # FLINK_SCAFFOLDING
 ##
-FLINK_SCAFFOLDING_JAR="${SOCSTREAM_HOME}/target/flink_scaffolding-1.0-jar-with-dependencies.jar"
+FLINK_SCAFFOLDING_JAR="${FLINK_SCAFFOLDING_HOME}/target/flink-scaffolding-1.0-jar-with-dependencies.jar"
 FLINK_SCAFFOLDING_QUERY="query-1"
 FLINK_SCAFFOLDING_OPTS="--port 9000"
 
